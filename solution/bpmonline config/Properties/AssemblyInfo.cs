0dc791e5831ee73d25676c34cc1b4b8e92cbc3bf
uakostya <uakostya@gmail.com>
2015-03-24 23:39:26 +0200

    Installer fix
    Can update to the new version, by running msi

--- solution/bpmonline config/Properties/AssemblyInfo.cs
+++ solution/bpmonline config/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BpmOnlineSiteConfigExtention")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("IIS Manager extention for configuring bpm'online web site settings")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BpmOnlineSiteConfigExtention")]
